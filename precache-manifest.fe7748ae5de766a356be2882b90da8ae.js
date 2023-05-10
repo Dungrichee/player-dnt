4808dcb1500aeb073f80a48decd9b4d1a5986e67
Nguyễn Thanh Dũng <dungnt@centic.vn>
2023-05-10 16:58:55 +0700

    Updates

---
+++ precache-manifest.fe7748ae5de766a356be2882b90da8ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d584a348cfded58c6eb0ec51858e73ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d269896d5ed6af571a20",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "453040a611be030f84cd",
+    "url": "./static/js/2.30d0af94.chunk.js"
+  },
+  {
+    "revision": "ec097c623f187c66ea7d98cd134f0c98",
+    "url": "./static/js/2.30d0af94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d269896d5ed6af571a20",
+    "url": "./static/js/main.af5baf60.chunk.js"
+  },
+  {
+    "revision": "9a823764ba7a94d49cb2",
+    "url": "./static/js/runtime-main.7d471726.js"
+  },
+  {
+    "revision": "2b2b7da40e9fe37746226683d444215a",
+    "url": "./static/media/getFetch.2b2b7da4.cjs"
+  }
+]);
